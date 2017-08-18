99b45114a822bd9f9e04bdbc5947b9174f7b25d0
darwinv <darwin.vasqz@gmail.com>
2017-08-18 01:27:00 -0400

    progress bar

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,6 +2,53 @@
 
 @section('content')
 
-<h1>Dashboard</h1>
+<h1>Tus Transacciones</h1>
+<div class="box-body">
+<p></p>
+<div class="callout callout-info">
+	<h3>Transacción en Progreso</h3>
+
+	<h4 >Tu cambio de Dolares a Soles esta en un 70%.</h4>
+
+<div class="progress active">	
+   <div class="progress-bar progress-bar-primary progress-bar-striped" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+      <span class="sr-only">70% Complete (success)</span>
+   </div>
+ </div>
+</div>
+
+
+ <div class="callout callout-success">
+    <h3>Transacción Exitosa</h3>
+	<h4 >Tu cambio de Euros a Bolivares fue exitosa</h4>
+	<div class="progress">
+    	<div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+      		<span class="sr-only">100% Complete</span>
+    	</div>
+  	</div>
+  </div>
+
+	<div class="callout callout-warning">
+                <h4>Cambiando</h4>
+
+                <p>Tu cambio de Reales a Libras estara en instantes.</p>
+		<div class="box box-warning">
+                    
+            <!-- /.box-body -->
+            <!-- Loading (remove the following to stop the loading)-->
+            <div class="overlay">
+              <i class="fa fa-refresh fa-spin"></i>
+            </div>
+            <!-- end loading -->
+          </div>
+
+     </div>
+	 
+
+ </div>
+
+
+
+              
 
 @endsection
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -243,7 +243,7 @@ desired effect
       <!-- search form (Optional) -->
       <form action="#" method="get" class="sidebar-form">
         <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <input type="text" name="q" class="form-control" placeholder="Buscar...">
               <span class="input-group-btn">
                 <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
                 </button>
@@ -268,7 +268,7 @@ desired effect
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Tus Transacciones
+        Dashboard
         <small>estado de las transacciones de cambio</small>
       </h1>
       <ol class="breadcrumb">
